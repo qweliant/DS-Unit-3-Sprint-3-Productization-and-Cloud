1b82d9caf93a9b2b990a4e0624e28b367be4a056
Qwelian Tanner <qweliant@gmail.com>
2019-11-12 14:36:38 -0500

    models set up for graphQL: TODO: add api functionality, homepage, model training

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from sqlalchemy import *
 from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
                             backref)
 from sqlalchemy.ext.declarative import declarative_base
+from flask_appbuilder.models.mixins import ImageColumn
+
 
 engine = create_engine('sqlite:///database.sqlite3', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -13,27 +15,28 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-class Department(Base):
-    __tablename__ = 'department'
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
 
 
-class Employee(Base):
-    __tablename__ = 'employee'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    hired_on = Column(DateTime, default=func.now())
-    department_id = Column(Integer, ForeignKey('department.id'))
-    department = relationship(
-        Department,
-        backref=backref('employees',
-                        uselist=True,
-                        cascade='delete,all'))
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 
 class Tweet(Base):
     __tablename__ = 'twitter'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    text = Column(Unicode(300))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(
+        User, 
+        backref=backref(
+            'tweets',  
+            lazy=True
+            ))
     
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
--- schema.py
+++ schema.py
@@ -2,30 +2,39 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import db_session, Department as DepartmentModel, Employee as EmployeeModel, Tweet as TweetModel
+from models import db_session, User as UserModel, Tweet as TweetModel
 
 
-class Department(SQLAlchemyObjectType):
+# class Department(SQLAlchemyObjectType):
+#     class Meta:
+#         model = DepartmentModel
+#         interfaces = (relay.Node, )
+
+
+# class DepartmentConnection(relay.Connection):
+#     class Meta:
+#         node = Department
+
+
+# class Employee(SQLAlchemyObjectType):
+#     class Meta:
+#         model = EmployeeModel
+#         interfaces = (relay.Node, )
+
+
+# class EmployeeCon(relay.Connection):
+#     class Meta:
+#         node = Employee
+
+class User(SQLAlchemyObjectType):
     class Meta:
-        model = DepartmentModel
+        model = UserModel
         interfaces = (relay.Node, )
 
 
-class DepartmentConnection(relay.Connection):
+class UserConnection(relay.Connection):
     class Meta:
-        node = Department
-
-
-class Employee(SQLAlchemyObjectType):
-    class Meta:
-        model = EmployeeModel
-        interfaces = (relay.Node, )
-
-
-class EmployeeCon(relay.Connection):
-    class Meta:
-        node = Employee
-
+        node = User
 
 
 class Tweet(SQLAlchemyObjectType):
@@ -34,7 +43,7 @@ class Tweet(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
-class TweetConnection(relay.Connection):
+class TweetCon(relay.Connection):
     class Meta:
         node = Tweet
 
@@ -42,10 +51,10 @@ class TweetConnection(relay.Connection):
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     # Allows sorting over multiple columns, by default over the primary key
-    all_employees = SQLAlchemyConnectionField(EmployeeCon)
+    # all_employees = SQLAlchemyConnectionField(EmployeeCon)
     # Disable sorting over this field
-    all_departments = SQLAlchemyConnectionField(DepartmentConnection, sort=None)
-
-    user = SQLAlchemyConnectionField(TweetConnection, sort=None)
+    # all_departments = SQLAlchemyConnectionField(DepartmentConnection, sort=None)
+    all_tweets = SQLAlchemyConnectionField(TweetCon, sort=None)
+    all_users = SQLAlchemyConnectionField(UserConnection, sort=None)
 
 schema = graphene.Schema(query=Query)
